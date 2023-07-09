ccc70ad5a2a71e1ad4816a2d25db52ae8e88ce07
Winpenning <zs.phenriqueprofessional@gmail.com>
2023-07-09 13:13:43 -0300

    Bases do projeto implementadas em Program.cs

--- ASPNETCore/Program.cs
+++ ASPNETCore/Program.cs
@@ -1,6 +1,22 @@
+// CONSTRUTOR DA APLICA플O
 var builder = WebApplication.CreateBuilder(args);
+
+// ADI플O DE SUPORTE A P핯INAS RAZOR
+builder.Services.AddRazorPages();
+
+// CONSTRUIR A APLICA플O
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// SERVIDOR HTTPS
+app.UseHttpsRedirection();
 
+// ARQUIVOS DO wwwroot
+app.UseStaticFiles();
+
+// SUPORTE PARA CRIAR URL's CUSTOMIZADAS
+// DAR UM NOME SIMPLES PARA UMA P핯INA, E O .NET CONSEGUIR ENCONTR�-LA DE MANEITA AUTOM햀ICA
+app.UseRouting();
+app.MapRazorPages();
+
+// EXECUTA A APLICA플O
 app.Run();
